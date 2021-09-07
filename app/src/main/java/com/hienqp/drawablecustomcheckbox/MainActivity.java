fc371f5f37fcddcdeed92b38bc183dd0a43d666a
hienqp <hienphan5290@gmail.com>
2021-09-08 06:27:11 +0700

    Drawable State List Custom Checkbox

---
+++ app/src/main/java/com/hienqp/drawablecustomcheckbox/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hienqp.drawablecustomcheckbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
